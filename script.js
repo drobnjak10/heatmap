68b2edbd912f0a70f143dc4a0ad87865d42937d7
drobnjak10 <drobnjakdavid@gmail.com>
2022-06-24 12:56:55 +0200

    =commit

--- script.js
+++ script.js
@@ -6,55 +6,27 @@ var heatmapInstance = h337.create({
 });
 
 function createHeatMapChart(x, y) {
+  heatmapInstance.setDataMax(50);
+
   heatmapInstance.addData({
     x,
     y,
-    value: 1,
+    value: 20,
   });
   //heatmapInstance.setData(data);
 }
 
-webgazer.setGazeListener(function(data, elapsedTime) {
-  if (data == null) {
+webgazer.setRegression("ridge");
+
+webgazer
+  .setGazeListener(function (data, elapsedTime) {
+    if (data == null) {
       return;
-  }
-  console.log(data)
-  createHeatMapChart(data.x, data.y);
-}).begin();
+    }
 
-//webgazer.showVideoPreview(false).showPredictionPoints(false);
+    var xprediction = data.x; //these x coordinates are relative to the viewport
+    var yprediction = data.y; //these y coordinates are relative to the viewport
 
-//const myfunc = () => {
-//  var heatmapInstance = h337.create({
-//    // only container is required, the rest will be defaults
-//    container: document.querySelector(".heatmap"),
-//  });
-
-//  // now generate some random data
-//  var points = [];
-//  var max = 0;
-//  var width = window.innerWidth;
-//  var height = window.innerHeight;
-//  var len = 50;
-
-//  let value = 1;
-//  while (len--) {
-//    var val = Math.floor(Math.random() * 100);
-//    value++;
-//    max = Math.max(max, val);
-//    var point = {
-//      value: val,
-//    };
-//    points.push(point);
-//  }
-//  // heatmap data format
-//  var data = {
-//    max: 20,
-//    data: arr,
-//  };
-//  // if you have a set of datapoints always use setData instead of addData
-//  // for data initialization
-//  heatmapInstance.setData(data);
-//};
-
-//myfunc();
+    createHeatMapChart(xprediction, yprediction);
+  })
+  .begin();
